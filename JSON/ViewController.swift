3ec2383e9ec75cb0cece0cfff7bad98954e06d4d
timothyjhull <timothyjhull@gmail.com>
2017-06-27 14:25:52 -0400

    Initial Commit

---
+++ JSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON
+//
+//  Created by Timothy Hull on 6/27/17.
+//  Copyright © 2017 Sponti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
